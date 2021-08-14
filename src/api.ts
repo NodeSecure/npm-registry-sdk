512b30d37a0da4a5f12bcf04fb0fb74e3941b44a
Ange Tekeu <tekeuangeleonel@gmail.com>
2021-08-14 21:57:12 +0100

    adding search funct in api.ts && with his utilities.

--- src/api.ts
+++ src/api.ts
@@ -1,54 +1,87 @@
 // Import Third-party Dependencies
-import * as npm from "@npm/types";
+import * as npm from '@npm/types';
 
 // Import Internal Dependencies
-import * as httpie from "@myunisoft/httpie";
-import { httpRegistryAgent, getLocalRegistryURL } from "./registry.js";
+import * as httpie from '@myunisoft/httpie';
+import { httpRegistryAgent, getLocalRegistryURL } from './registry.js';
+import { clamp } from './utils.js';
+
+/***************************************************************************************************** */
+
+/***************************************************************************************************** */
 
 export interface NpmRegistryMetadata {
-  db_name: string;
-  doc_count: number;
-  doc_del_count: number;
-  update_seq: number;
-  purge_seq: number;
-  compact_running: boolean;
-  disk_size: number;
-  data_size: number;
-  instance_start_time: string;
-  disk_format_version: number;
-  committed_update_seq: number;
+	db_name: string;
+	doc_count: number;
+	doc_del_count: number;
+	update_seq: number;
+	purge_seq: number;
+	compact_running: boolean;
+	disk_size: number;
+	data_size: number;
+	instance_start_time: string;
+	disk_format_version: number;
+	committed_update_seq: number;
 }
 
 export async function metadata(): Promise<NpmRegistryMetadata> {
-  const { data } = await httpie.get<NpmRegistryMetadata>(getLocalRegistryURL(), {
-    agent: httpRegistryAgent
-  });
+	const { data } = await httpie.get<NpmRegistryMetadata>(getLocalRegistryURL(), {
+		agent: httpRegistryAgent,
+	});
 
-  return data;
+	return data;
 }
 
 export interface PackumentOptions {
-  token: string;
+	token: string;
 }
 
 export async function packument(name: string, options?: PackumentOptions): Promise<npm.Packument> {
-  const path = new URL(name, getLocalRegistryURL());
+	const path = new URL(name, getLocalRegistryURL());
 
-  const { data } = await httpie.get<npm.Packument>(path, {
-    authorization: options?.token,
-    agent: httpRegistryAgent
-  });
+	const { data } = await httpie.get<npm.Packument>(path, {
+		authorization: options?.token,
+		agent: httpRegistryAgent,
+	});
 
-  return data;
+	return data;
 }
 
 export async function packumentVersion(name: string, version: string, options?: PackumentOptions): Promise<npm.PackumentVersion> {
-  const path = new URL(`${name}/${version}`, getLocalRegistryURL());
+	const path = new URL(`${name}/${version}`, getLocalRegistryURL());
 
-  const { data } = await httpie.get<npm.PackumentVersion>(path, {
-    authorization: options?.token,
-    agent: httpRegistryAgent
-  });
+	const { data } = await httpie.get<npm.PackumentVersion>(path, {
+		authorization: options?.token,
+		agent: httpRegistryAgent,
+	});
 
-  return data;
+	return data;
+}
+
+export async function search(searchOption) {
+	const query = new URL(`${this.url}/-/v1/search`);
+
+	// Apply options to the URL
+	const { text, size, from, quality, popularity, maintenance } = searchOption;
+	if (typeof text === 'string') {
+		query.searchParams.set('text', text);
+	}
+	if (typeof size === 'number') {
+		query.searchParams.set('size', String(clamp(size, 0, 250)));
+	}
+	if (typeof from === 'number') {
+		query.searchParams.set('from', String(from));
+	}
+	if (typeof quality === 'number') {
+		query.searchParams.set('quality', String(clamp(quality, 0, 1)));
+	}
+	if (typeof popularity === 'number') {
+		query.searchParams.set('popularity', String(clamp(popularity, 0, 1)));
+	}
+	if (typeof maintenance === 'number') {
+		query.searchParams.set('maintenance', String(clamp(maintenance, 0, 1)));
+	}
+
+	// Send the Query
+	return (await httpie.get(query.href, { headers: this.headers })).data;
 }
