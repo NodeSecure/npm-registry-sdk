2a8b42d115c86c1d381f501cb5e84e20a6be4b69
cgombauld <clement.gombauld@cpexterne.org>
2025-10-11 22:55:20 +0200

    feat(api): implement org api

---
+++ src/api/org.ts
@@ -0,0 +1,19 @@
+// Import Third-party Dependencies
+import * as httpie from "@openally/httpie";
+
+// Import Internal Dependencies
+import { getNpmRegistryURL } from "../registry.js";
+
+export type PermissionLevel = "read" | "write" | "admin" | "maintain";
+
+export type NpmPackageOrg = Record<string, PermissionLevel>;
+
+export async function org(
+  namespace: string
+): Promise<NpmPackageOrg> {
+  const url = new URL(`-/org/${namespace}/package`, getNpmRegistryURL());
+
+  const { data } = await httpie.get<NpmPackageOrg>(url);
+
+  return data;
+}
